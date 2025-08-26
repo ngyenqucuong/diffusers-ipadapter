8ead481e0f9595a7af57b34ba857b095fc1ab315
Nguyễn Quốc Cường <104240677@student.vgu.edu.vn>
2025-08-26 10:26:19 +0700

    edit

--- app.py
+++ app.py
@@ -235,14 +235,13 @@ async def gen_img2img(job_id: str, face_image : Image.Image,pose_image: Image.Im
     #     generator=generator,
     #     num_images_per_prompt=1
     # ).images[0]
-    print(request.controlnet_conditioning_scale)
     generated_image = pipe(
         prompt=request.prompt,
         negative_prompt=negative_prompt,
         image_embeds=face_emb,
         image=pose_kps,
         control_image=pose_kps,
-        controlnet_conditioning_scale=request.controlnet_conditioning_scale,
+        controlnet_conditioning_scale=float(request.controlnet_conditioning_scale),
         ip_adapter_scale=request.ip_adapter_scale,
         num_inference_steps=request.num_inference_steps,
         guidance_scale=request.guidance_scale,
